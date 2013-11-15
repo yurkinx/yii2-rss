99114f54a05f460f7097dad4ed5099e217352df5
Yuri Kileev <kileev@gmail.com>
2013-11-14 22:13:00 -0500

    Add FeedDriver

--- yii/feed/FeedDriver.php
+++ yii/feed/FeedDriver.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Image.php
+ * FeedDriver.php
  * @author: Yuri Kileev <kileev@gmail.com>
- * @date  : 17.10.2013
+ * @date  : 14.11.2013
  */
 
 namespace yii\feed;
@@ -14,33 +14,24 @@ use Zend\Feed\Writer\Writer;
 use Zend\Feed\Reader\Reader;
 
 /**
- * Class ImageDriver
- * The main class to wrap Kohana Image Extension
- * @package yii\image
+ * Class FeedDriver
+ * The main class to wrap Zend Feed Extension
+ * @package yii\feed
  */
 class FeedDriver extends Component
 {
         /**
-         * Loads the image to Kohana_Image object
-         * @param string $file the file path to the image
-         * @param string $driver the image driver to use: GD or ImageMagick
-         * @throws ErrorException if filename is empty or file doesn't exist
-         * @return mixed object Image_GD or object Image_Imagick 
+         * Loads read Zend-feed component
+         * @return mixed object Zend\Feed\Reader component
          */
-        public function load($file = null, $driver = null){
-            
-            if(empty($file)){
-                 throw new ErrorException('File name can not be empty');
-            }
-            if(!realpath($file)){
-                 throw new ErrorException(sprintf('The file doesn\'t exist: %s',$file));
-            }
-            return Image::factory($file, $driver ? $driver : $this->driver);     
-        }
         public function reader(){
             
             return new Reader;
         }
+        /**
+         * Loads read Zend-feed component
+         * @return mixed object Zend\Feed\Writer component
+         */
         public function writer(){
             
             return new Writer;
